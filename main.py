0e40e0cc81ba3316188010af309d8ad28df8c883
sasit622 <sasit622@gmail.com>
2026-08-25 15:05:02 +0530

    Add FastAPI backend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI,Body,Form,UploadFile,File,Query,Path,APIRouter
 from pydantic import BaseModel
 from typing import List
-from src.router.speed_route import router as speed_router 
-from src.router.user_route import router as user_router
+from Backend.router.speed_route import router as speed_router 
+from Backend.router.user_route import router as user_router
 app=FastAPI()
 app.include_router(user_router)
 app.include_router(speed_router)
--- src/model/user.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-class UserResponse(BaseModel):
-    exists:bool
-
-class ResUser(BaseModel):
-    category:str
-    interest:str
-    recom:List[str]
